8adb127d8761936fc918facb2c392b818e70c54d
Max Grüttner <maxgruttner@gmail.com>
2026-06-28 13:03:07 +0200

    vervolltändigen der beginner sachen in lecture 0

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("hello, world")
+###Lecture 0
+print("hello, world") #Erstes Projekt jedes Beginners
+
+#Into Functions and Variables
+name = input("What is your name? ").strip().title()
+
+#name = name.strip().title() #Entfernt Leerzeichen von str
+#Großschreiben des ersten Buchstabens jedes Wortes .title
+# Wenn nur Großschreiben des erstes Buchstabens des ersten Worten .capitalize
+
+print(f"Hello {name}")

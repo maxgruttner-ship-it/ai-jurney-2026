67fa6b2f16937531797db9b5e5534469f0895ba5
Max Grüttner <maxgruttner@gmail.com>
2026-07-08 11:48:50 +0200

    own_func

--- calculator.py
+++ calculator.py
@@ -1,6 +1,8 @@
-x = 1
-y = 2
+def main():
+    x = int(input("Was ist x? "))
+    print("x quadriert ergibt", square(x))
 
-z = x + y
+def square(n):
+    return n ** 2
 
-print(z)
+main()
---
+++ hello_func.py
@@ -0,0 +1,8 @@
+def main():
+    name = input("Was ist dein Name? ")
+    hello(name)
+
+def hello(zu="Welt"):
+    print("Hallo", zu)
+
+main()
